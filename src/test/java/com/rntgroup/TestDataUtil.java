d908ff27e5a80ca4c2a4a321fb9beba6756fddd3
Stepan_Klimashin <stepan_klimashin@rntgroup.com>
2022-10-05 22:09:06 +0400

    Добавлены тесты для TicketController

--- src/test/java/com/rntgroup/TestDataUtil.java
+++ src/test/java/com/rntgroup/TestDataUtil.java
@@ -1,6 +1,7 @@
 package com.rntgroup;
 
 import com.rntgroup.model.Event;
+import com.rntgroup.model.Ticket;
 import com.rntgroup.model.User;
 import lombok.experimental.UtilityClass;
 import org.jeasy.random.EasyRandom;
@@ -22,6 +23,23 @@ public class TestDataUtil {
         parameters.excludeField(FieldPredicates.named("id").and(FieldPredicates.inClass(Event.class)));
 
         return new EasyRandom(parameters).nextObject(User.class);
+    }
+
+    public Ticket getRandomTicket() {
+        var parameters = new EasyRandomParameters();
+        parameters.excludeField(FieldPredicates.named("userId")
+                .and(FieldPredicates.named("eventId"))
+                .and(FieldPredicates.inClass(Ticket.class)));
+
+        return new EasyRandom(parameters).nextObject(Ticket.class);
+    }
+
+    public Ticket getRandomBookedTicket(long eventId, long userId) {
+        Ticket ticket = getRandomTicket();
+        ticket.setEventId(eventId);
+        ticket.setUserId(userId);
+
+        return ticket;
     }
 
 }
